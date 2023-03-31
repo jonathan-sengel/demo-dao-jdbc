774cec0e8ab285746fad5ac4512c317830d75b56
Jonathan Engel <joow.engeel@gmail.com>
2023-03-30 23:51:32 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,11 +20,8 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(21, "Bob Cat", "bob@mail.com", new Date(), 2846.00, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
-
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,15 +4,25 @@
  */
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import java.util.List;
 import model.dao.SellerDao;
 import model.entities.Seller;
+import java.sql.*;
+import model.entities.Department;
 
 /**
  *
  * @author Jow
  */
-public class SellerDaoJDBC implements SellerDao{
+public class SellerDaoJDBC implements SellerDao {
+
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
 
     @Override
     public void insert(Seller obj) {
@@ -31,12 +41,48 @@ public class SellerDaoJDBC implements SellerDao{
 
     @Override
     public Seller findById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(findSellerById(id));
+            rs = st.executeQuery();
+            if (rs.next()) {
+                Department dept = new Department();
+                dept.setId(rs.getInt("department_id"));
+                dept.setName(rs.getString("dept_name"));
+
+                Seller seller = new Seller();
+                seller.setId(rs.getInt("id"));
+                seller.setName(rs.getString("name"));
+                seller.setEmail(rs.getString("email"));
+                seller.setBaseSalary(rs.getDouble("base_salary"));
+                seller.setBirthDate(rs.getDate("birth_date"));
+                seller.setDepartment(dept);
+
+                return seller;
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+        }
     }
 
     @Override
     public List<Seller> findAll() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
+    private String findSellerById(Integer id) {
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT  seller.*, ");
+        sql.append("        department.name AS dept_name ");
+        sql.append("FROM seller ");
+        sql.append("INNER JOIN department ON seller.department_id = department.id ");
+        sql.append("WHERE seller.id = ").append(id);
+        return sql.toString();
+    }
+
 }
