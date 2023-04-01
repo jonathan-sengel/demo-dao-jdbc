89268eb97a658901b17183a9f570e51f57b6193e
Jonathan Engel <joow.engeel@gmail.com>
2023-04-01 15:12:08 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,10 +41,17 @@ public class Program {
         System.out.println("total rows: " + list.size());
 
         System.out.println();
-        System.out.println(">>>>>> TEST 3: seller insert <<<<<<");
+        System.out.println(">>>>>> TEST 4: seller insert <<<<<<");
         Seller newSeller = new Seller(null, "Greg Silva", "greg@mail.com", new Date(), 1850.00, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted. New id = " + newSeller.getId());
+        
+        System.out.println();
+        System.out.println(">>>>>> TEST 5: seller update <<<<<<");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -58,7 +58,22 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void update(Seller obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(updateSeller());
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setInt(6, obj.getId());
+
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
@@ -146,9 +161,16 @@ public class SellerDaoJDBC implements SellerDao {
         sql.append("INSERT INTO seller");
         sql.append(" (name, email, birth_date, base_salary, department_id)");
         sql.append(" VALUES (?,?,?,?,?)");
-
         return sql.toString();
 
+    }
+
+    private String updateSeller() {
+        StringBuilder sql = new StringBuilder();
+        sql.append("UPDATE seller");
+        sql.append(" SET name=?, email=?, birth_date=?, base_salary=?, department_id=?");
+        sql.append(" WHERE id = ?");
+        return sql.toString();
     }
 
     private String findSellerById(Integer sellerId) {
